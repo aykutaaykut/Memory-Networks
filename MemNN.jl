db03561433faa3a25328c27df87e295d31d8605b
aykutaaykut <aykut09aykut@gmail.com>
2017-03-22 03:28:49 +0300

    Initial commit

---
+++ MemNN.jl
@@ -0,0 +1,91 @@
+using Knet, AutoGrad, Compat, ArgParse
+
+function parse_commandline()
+  s = ArgParseSettings()
+  @add_arg_table s begin
+    ("--datafiles"; nargs = '+'; help = "If provided, use first file for training, second for dev, others for test.")
+    ("--input"; arg_type = Char; default = 's'; help = "s for sentence sequences, w for word sequences as input")
+    ("--batchsize"; arg_type=Int; default=90; help="Number of sequences to train on in parallel.")
+    ("--winit"; arg_type=Float64; default=0.1; help="Initial weights set to winit*randn().")
+    ("--seed"; arg_type=Int; default=38; help="Random number seed.")
+    ("--atype"; default = (gpu() >= 0 ? "KnetArray{Float32}" : "Array{Float32}"); help = "Array type: Array for CPU, KnetArray for GPU")
+  end
+  return parse_args(s; as_symbols = true)
+end
+
+function main(args = ARGS)
+  #Fixed configurations for baseline model
+  embedding_dimension = 100
+  learning_rate = 0.01
+  margin = 0.1
+  total_epochs = 10
+
+  settings = parse_commandline()
+  println("settings", [(symbol, value) for (symbol, value) in settings]...)
+  settings[:seed] > 0 && srand(opts[:seed])
+  settings[:atype] = eval(parse(settings[:atype]))
+  #Classification of datafiles as training, dev and test
+  training_data = settings[:datafiles][1]
+  dev_data = settings[:datafiles][2]
+  test_data = settings[:datafiles][3:end]
+
+  dict = createDict(training_data)
+  dict_length = length(dict)
+  info("$dict_length unique words.")
+  feature_space = 3 * dict_length
+
+  model = initWeights(settings[:atype], feature_space, embedding_dimension, settings[:winit])
+
+end
+
+function createDict(training_data)
+  dict = Dict{String, Int}
+  number = 1
+  open(training_data) do f
+    while !eof(f)
+      str = readline(f)
+      number, dict = parseLineAddDict(number, str, dict)
+    end
+  end
+  return dict
+end
+
+function parseLineAddDict(number, line, dict)
+  words = split(line)
+  for str in words
+    if !haskey(dict, str)
+      dict[str] = number
+      number += 1
+    end
+  end
+  return (number, dict)
+end
+
+function initWeights(atype, feature_space, embedding_dimension, winit)
+  weights = Dict()
+
+  uo = winit * randn(embedding_dimension, feature_space)
+  ur = winit * randn(embedding_dimension, feature_space)
+  weights[:uo] = uo
+  weights[:ur] = ur
+
+  for k in keys(weights)
+    weights[k] = convert(atype, weights[k])
+  end
+  return weights
+end
+
+function phi(x)
+
+end
+
+function s(x, y, u)
+  score = 0
+  phiy = phi(y)
+  for input in x
+    phix = phi(input)
+    current_score = phix' * u' * u * phiy
+    score = score + current_score
+  end
+  return score
+end
